543a101f09d8d6a6f097bec8b392717883374c12
James Keicher <jamesdkeicher@gmail.com>
2022-02-14 15:01:53 -0800

    feat: pokemonSearch component fetches pokemon by name using data from form

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Spinner />
       <PokemonSearch />
       <YelpSearch />
       {/* stretch goal: <WeatherSearch /> */}
